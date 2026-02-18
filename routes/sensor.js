d292c2a10bf064656a1149874b89fcc694c9c511
Anis Boulfoul <a.boulfoul@esi-sba.dz>
2026-02-18 11:01:13 +0100

    V5: Add water pump feature

--- routes/sensor.js
+++ routes/sensor.js
@@ -4,11 +4,10 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.json({
-<<<<<<< HEAD
-	status: "active"
         soilMoisture: "40%",
         temperature: "27°C",
         humidity: "60%",
+        status: "Optimal"
     });
 });
 
